05bb54a1c2368851ebec107c276d8c83fa736555
Wai-Shing Luk <luk036@gmail.com>
2022-05-11 21:31:13 +0800

    fix regression

--- tests/test_sp_n.py
+++ tests/test_sp_n.py
@@ -6,7 +6,7 @@ from lds_py.discrep_2 import discrep_2
 from lds_py.lds_n import CylinN, SphereN
 
 
-def run_pylds(spgen):
+def run_lds(spgen):
     npoints = 600
     Triples = np.array([spgen.pop() for _ in range(npoints)])
     hull = ConvexHull(Triples)
@@ -16,7 +16,7 @@ def run_pylds(spgen):
 
 def test_sphere_n():
     spgen = SphereN(3, [2, 3, 5, 7])
-    measure = run_pylds(spgen)
+    measure = run_lds(spgen)
     assert measure == approx(0.9125914)
     # assert measure < 0.913
     # assert measure > 0.912
@@ -24,7 +24,7 @@ def test_sphere_n():
 
 def test_cylin_n():
     cygen = CylinN(3, [2, 3, 5, 7])
-    measure = run_pylds(cygen)
+    measure = run_lds(cygen)
     assert measure == approx(1.0505837105828988)
     # assert measure < 1.086
     # assert measure > 1.085
